a8f9418cd04777227555dbc86fb0978ced5f5e13
Hoang Duy Nguyen <kiemda02@gmail.com>
2021-09-16 10:55:27 +0700

    fix Linear Equation Resolver

--- src/LinearEquationResolver/LinearEquationResolver.java
+++ src/LinearEquationResolver/LinearEquationResolver.java
@@ -4,13 +4,14 @@ import java.util.Scanner;
 
 public class LinearEquationResolver {
     public static void main(String[] args) {
-        double a;
-        double b;
+        System.out.println("Linear Equation Resolver");
+        System.out.println("Given a equation as 'a * x + b = 0, please enter constants:");
+
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter number a:");
-        a = scanner.nextDouble();
+        double a = scanner.nextDouble();
         System.out.println("Enter number b:");
-        b = scanner.nextDouble();
+        double b = scanner.nextDouble();
         if (a == 0 && b == 0) {
             System.out.println("The solution is all");
         } else {
